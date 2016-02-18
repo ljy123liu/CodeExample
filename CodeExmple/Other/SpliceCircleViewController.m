87b56caa5f75506146094a5db51cf1d01a879b7f
LIUYONG <liuyong@liuyongs-mbp.lan>
2016-02-18 16:40:09 +0800

    只选两种圆的情况

--- CodeExmple/Other/SpliceCircleViewController.m
+++ CodeExmple/Other/SpliceCircleViewController.m
@@ -18,20 +18,27 @@
     [super viewDidLoad];
     self.title = @"SpliceCircle";
     self.view.backgroundColor = [UIColor whiteColor];
-    [self showCircle:20 x1:self.view.centerX y1:self.view.centerY];
+    [self showCircle:20 d1:20 d2:40];
 }
-
-- (void)showCircle:(int)n x1:(double)x1 y1:(double)y1 {
+//只选两种圆的情况
+- (void)showCircle:(int)n d1:(double)d1 d2:(double)d2 {
+    
+    double r1 = d1/2;
+    double r2 = d2/2;
     //l:间距
-    double l = 32;
+    double l = r1+r2+1;
+    //大圆半径
     double someR = l * sin((n-2) * M_PI / (2*n)) / sin(2*M_PI/n);
+//    NSLog(@"%f",someR);
     
     for (int i = 0; i < n; ++i) {
+        
         double x = someR * sin(i*2*M_PI/n);
         double y = someR * cos(i*2*M_PI/n);
-        double r = i % 2 ? 10 : 20;
+        double r = i % 2 ? r1 : r2;
+        
         UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 2*r, 2*r)];
-        view.center = CGPointMake(x+x1, y+y1);
+        view.center = CGPointMake(x+self.view.centerX, y+self.view.centerY);
         view.clipsToBounds = YES;
         view.layer.cornerRadius = r;
         view.backgroundColor = [UIColor redColor];
